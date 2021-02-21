75b22dff714c6bd41089d2a1bc81c8b38668a09a
Avvessalom <avvessalom@gmail.com>
2021-02-21 12:27:05 +0300

    final scripts

--- scripts/zfs_mount.sh
+++ scripts/zfs_mount.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-device=$(losetup -f)
 dd if=/dev/zero of=fs.img bs=1k count=66000
 dd if=/dev/zero of=fs1.img bs=1k count=66000
-zpool create myzfs fs.img fs1.img
+zpool create myzfs /home/kain/github/ITMO-System-level-Software/scripts/fs.img /home/kain/github/ITMO-System-level-Software/scripts/fs1.img
 zfs create myzfs/test1
-#losetup "$device" fs.img
-#mkfs -c -T zfs device
-#mkdir /mnt/zfs
-#mount -t zfs "$device" /mnt/zfs
+echo "---------------------------------------------------------------------------------"
+zfs list
--- scripts/zfs_unmount.sh
+++ scripts/zfs_unmount.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+rm -rf *.img
 zfs destroy myzfs/test1
+zpool destroy myzfs
+zfs list
