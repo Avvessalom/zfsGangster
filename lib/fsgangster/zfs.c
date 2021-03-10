1b2e6c69c80e75b277bd3ad7688a307959e86b3d
Avvessalom <avvessalom@gmail.com>
2021-03-10 19:30:45 +0300

    add makefile

--- lib/fsgangster/zfs.c
+++ lib/fsgangster/zfs.c
@@ -2,12 +2,15 @@
 // Created by kain on 21.02.2021.
 //
 #include <stdio.h>
+#include <byteswap.h>
+
 #include "zfs.h"
 
 #define BLOCK_SIZE 512
 
 int openZfs(const char* path){
     FILE* file = fopen(path, "rb");
+    long int n;
     struct uberblock block;
     if (!file){
         perror("Cannot open\n");
@@ -17,7 +20,9 @@ int openZfs(const char* path){
     fseek(file, 0x28000, SEEK_SET);
 
     fread(&block, sizeof(block), 1, file);
-
+    n = bswap_64(block.ub_magic);
+    printf("%lu\n", block.ub_magic);
+    printf("%ld", n);
     fclose(file);
     return 0;
 }
