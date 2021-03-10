b08bb413097e3b8ca5d77fe87e7190dad95334f5
avvessalom <avvessalom@gmail.com>
2021-03-10 22:55:27 +0300

    update scripts

--- scripts/zfs_mount.sh
+++ scripts/zfs_mount.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 dd if=/dev/zero of=fs.img bs=1M count=101
-zpool create myzfs -o ashift=9 /home/kain/github/ITMO-System-level-Software/scripts/fs.img
+zpool create myzfs -o ashift=9 /home/kain/github/zfsGangster/scripts/fs.img
 zfs set recordsize=512 myzfs
 echo "---------------------------------------------------------------------------------"
 zfs list
--- scripts/zfs_unmount.sh
+++ scripts/zfs_unmount.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 rm -rf *.img
-zfs destroy myzfs/test1
 zpool destroy myzfs
 zfs list
