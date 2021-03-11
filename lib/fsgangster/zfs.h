c1c672a887da9ef417bd25ba7f404c0799d52535
avvessalom <avvessalom@gmail.com>
2021-03-11 22:42:24 +0300

    true vdev structs

--- lib/fsgangster/zfs.h
+++ lib/fsgangster/zfs.h
@@ -20,26 +20,38 @@
 	sizeof (zio_block_tail_t) - \
 	(SPA_GBH_NBLKPTRS * sizeof (blkptr_t))) /\
 	sizeof (uint64_t))
-
-typedef struct vdev_label {
-    char                vl_pad[VDEV_SKIP_SIZE];             /*  8K  */
-    vdev_boot_header_t  vl_boot_header;                     /*  8K под вопросиком в фрибсд нету такого */
-    vdev_phys_t         vl_vdev_phys;                       /* 112K */
-    char                vl_uberblock[VDEV_UBERBLOCK_RING];  /* 128K */
-} vdev_label_t;
-
-typedef struct dva {
-    uint64_t dva_word[2];
-} dva_t;
-
+//------------------------------------------------------------------------------
 typedef struct zio_cksum {
     uint64_t zc_word[4];
 } zio_cksum_t;
 
 typedef struct zio_eck {
-    uint64_t        zec_magic;
-    zio_cksum_t     zec_cksum;
+    uint64_t	zec_magic;	/* for validation, endianness	*/
+    zio_cksum_t	zec_cksum;	/* 256-bit checksum		*/
 } zio_eck_t;
+
+typedef struct vdev_boot_envblock {
+    uint64_t	vbe_version;
+    char		vbe_bootenv[VDEV_SKIP_SIZE - sizeof (uint64_t)
+                            sizeof (zio_eck_t)];
+    zio_eck_t	vbe_zbt;
+} vdev_boot_envblock_t;
+
+typedef struct vdev_phys {
+    char		vp_nvlist[VDEV_PHYS_SIZE - sizeof (zio_eck_t)];
+    zio_eck_t	vp_zbt;
+} vdev_phys_t;
+
+typedef struct  vdev_label {
+    char                vl_pad[VDEV_SKIP_SIZE];             /*  8K  */
+    vdev_boot_envblock_t	vl_be;                     /*  8K под вопросиком в фрибсд нету такого */
+    vdev_phys_t         vl_vdev_phys;                       /* 112K */
+    char                vl_uberblock[VDEV_UBERBLOCK_RING];  /* 128K */
+} vdev_label_t;
+//-----------------------------------------------------------------------------------
+typedef struct dva {
+    uint64_t dva_word[2];
+} dva_t;
 
 typedef struct blkptr {
     dva_t    blk_dva[3];            /* 128-bit Data Virtual Address */
@@ -51,7 +63,7 @@ typedef struct blkptr {
 } blkptr_t;
 
 
-struct __attribute__((packed)) uberblock {
+struct uberblock {
     uint64_t ub_magic;              /* UBERBLOCK_MAGIC		    */
     uint64_t ub_version;            /* SPA_VERSION			    */
     uint64_t ub_txg;                /* txg of last sync		    */
@@ -65,11 +77,6 @@ typedef struct zio_block_tail {
     uint64_t	zbt_magic;	        /* for validation, endianness	*/
     zio_cksum_t	zbt_cksum;	        /* 256-bit checksum		*/
 } zio_block_tail_t;
-
-typedef struct vdev_phys {
-    char vp_nvlist[VDEV_PHYS_SIZE - sizeof (zio_block_tail_t)];
-    zio_block_tail_t vp_zbt;
-} vdev_phys_t;
 
 typedef struct zio_gbh {
     blkptr_t            zg_blkptr[SPA_GBH_NBLKPTRS];
