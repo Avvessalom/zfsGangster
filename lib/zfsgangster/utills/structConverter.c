402dfa0353fc5d884f3b64b01637ca9e4e21057f
avvessalom <avvessalom@gmail.com>
2021-03-25 12:42:39 +0300

    update converter

--- lib/zfsgangster/utills/structConverter.c
+++ lib/zfsgangster/utills/structConverter.c
@@ -2,9 +2,8 @@
 // Created by kain on 10.03.2021.
 //
 #include <byteswap.h>
-
-#include "../zfs.h"
-
+#include "structConverter.h"
+#include "../include/uberblock.h"
 
 void convert_zio_chksum(struct zio_cksum* cksum){
     for (int i = 0; i < 4; i++){
@@ -36,36 +35,36 @@ void convert_uberblock(struct uberblock* block){
     block->ub_software_version = bswap_64(block->ub_software_version);
     convert_blkptr(&block->ub_rootbp);
 }
-
-void convert_block_tail(struct zio_block_tail* tail){
-    tail->zbt_magic = bswap_64(tail->zbt_magic);
-    convert_zio_chksum(&tail->zbt_cksum);
-}
-
-void convert_zio_gbn(struct zio_gbh* gbh){
-    convert_blkptr(&gbh->zg_blkptr);
-    gbh->zg_filler = bswap_64(gbh->zg_filler);
-    convert_block_tail(&gbh->zg_tail);
-}
-
-void convert_dnode_phys(struct dnode_phys* dnode){
-    dnode->dn_maxblkid = bswap_64(dnode->dn_maxblkid);
-    dnode->dn_secphys = bswap_64(dnode->dn_secphys);
-    for (int i = 0; i < 3; i++){
-        dnode->dn_pad3[i] = bswap_64(dnode->dn_pad3);
-    }
-    convert_blkptr(&dnode->dn_blkptr);
-}
-
+//
+//void convert_block_tail(struct zio_block_tail* tail){
+//    tail->zbt_magic = bswap_64(tail->zbt_magic);
+//    convert_zio_chksum(&tail->zbt_cksum);
+//}
+//
+//void convert_zio_gbn(struct zio_gbh* gbh){
+//    convert_blkptr(&gbh->zg_blkptr);
+//    gbh->zg_filler = bswap_64(gbh->zg_filler);
+//    convert_block_tail(&gbh->zg_tail);
+//}
+//
+//void convert_dnode_phys(struct dnode_phys* dnode){
+//    dnode->dn_maxblkid = bswap_64(dnode->dn_maxblkid);
+//    dnode->dn_secphys = bswap_64(dnode->dn_secphys);
+//    for (int i = 0; i < 3; i++){
+//        dnode->dn_pad3[i] = bswap_64(dnode->dn_pad3);
+//    }
+//    convert_blkptr(&dnode->dn_blkptr);
+//}
+//
 void convert_zio_eck(struct zio_eck* eck){
     eck->zec_magic = bswap_64(eck->zec_magic);
     convert_zio_chksum(&eck->zec_cksum);
 }
+//
+//void convert_vdev_phys(struct vdev_phys* phys){
+//    convert_zio_eck(&phys->vp_zbt);
+//}
 
-void convert_vdev_phys(struct vdev_phys* phys){
-    convert_zio_eck(&phys->vp_zbt);
-}
-
-void convert_vdev_label(struct vdev_label label){
-    convert_dnode_phys(&label.vl_vdev_phys);
-}
+//void convert_vdev_label(struct vdev_label label){
+//    convert_dnode_phys(&label.vl_vdev_phys);
+//}
--- lib/zfsgangster/utills/structConverter.h
+++ lib/zfsgangster/utills/structConverter.h
@@ -5,4 +5,12 @@
 #ifndef ZFSGANGSTER_STRUCTCONVERTER_H
 #define ZFSGANGSTER_STRUCTCONVERTER_H
 
+#include "../include/vdev.h"
+#include "../include/uberblock.h"
+
+void convert_zio_chksum(struct zio_cksum* cksum);
+void convert_blkptr(struct blkptr* blk);
+void convert_uberblock(struct uberblock* block);
+void convert_zio_eck(struct zio_eck* eck);
+
 #endif //ZFSGANGSTER_STRUCTCONVERTER_H
